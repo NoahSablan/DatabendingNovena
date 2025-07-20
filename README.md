04f7e3b56de205e4afe10bb3cfa76c662c935e16
NoahSablan <78834803+NoahSablan@users.noreply.github.com>
2025-07-20 19:24:07 -0400

    Initial Commit of local files

---
+++ README.md
@@ -0,0 +1,36 @@
+# README
+
+All Images in this source directory are not owned by me and are meant to be example only.
+
+### Re-editioning an old prayer book
+
+
+
+This project is a WIP. This project seeks to explore how cultural memory is intertwined with documentation. The images produced using this code are meant to be included as illustrations in a personal re-editioning of a prayer book used throughout the Catholic households in Guam. The images are intended to come into dialogue with the CHamoru Novena, which is considered one of the first published texts in the CHamoru language.
+
+
+
+#### Databending
+
+
+
+Databending is a unique way to intentionally corrupt and change images. The project uses Audacity and the Audacity API to "data-bend" raw image files using audio tools. 
+
+
+
+
+
+#### Novena
+
+
+
+Due to colonial pressures in the 20th century, CHamoru language texts were very late to come into existence. As the American military pressured locals to assimilate, generations of CHamorus lost the language in daily use. The novena, a catholic prayer book, was published in CHamoru in 1928. By the end of the century, it had found it's way into almost every household in Guam, which to me, is a manifestation of complete cultural assimilation into Catholicism.
+
+
+
+The book recounts the days leading up to the birth of Jesus. It holds themes of motherhood, family, and reciprocity which are key elements of CHamoru culture. At the same time, it contains diction and story elements that embolden obedience, subservience and subjugation under higher powers. 
+
+
+
+By incorporating data-bent images with the story presented in the Novena, I hope to reconcile which ideas we should keep and which we should discard from this common household artifact.
+
---
+++ src/bulkDatabend.py
@@ -0,0 +1,103 @@
+# Original code from Audacity's mod-script-pipe.
+# Modified to perform commands on raw image files in bulk.
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests the audacity pipe.
+
+Keep pipe_test.py short!!
+You can make more complicated longer tests to test other functionality
+or to generate screenshots etc in other scripts.
+
+Make sure Audacity is running first and that mod-script-pipe is enabled
+before running this script.
+
+Requires Python 2.7 or later. Python 3 is strongly recommended.
+
+"""
+
+import os
+import sys
+import time
+
+# Set up pipe
+if sys.platform == 'win32':
+    print("bulkDatabend.py, running on windows")
+    TONAME = '\\\\.\\pipe\\ToSrvPipe'
+    FROMNAME = '\\\\.\\pipe\\FromSrvPipe'
+    EOL = '\r\n\0'
+else:
+    print("bulkDatabend.py, running on linux or mac")
+    TONAME = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
+    FROMNAME = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+    EOL = '\n'
+
+print("Write to  \"" + TONAME +"\"")
+if not os.path.exists(TONAME):
+    print(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
+    sys.exit()
+
+print("Read from \"" + FROMNAME +"\"")
+if not os.path.exists(FROMNAME):
+    print(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
+    sys.exit()
+
+print("-- Both pipes exist.  Good.")
+
+# Open
+TOFILE = open(TONAME, 'w')
+print("-- File to write to has been opened")
+FROMFILE = open(FROMNAME, 'rt')
+print("-- File to read from has now been opened too\r\n")
+
+# Test Send
+def send_command(command):
+    """Send a single command."""
+    print("Send: >>> \n"+command)
+    TOFILE.write(command + EOL)
+    TOFILE.flush()
+
+# Test Response
+def get_response():
+    """Return the command response."""
+    result = ''
+    line = ''
+    while True:
+        result += line
+        line = FROMFILE.readline()
+        if line == '\n' and len(result) > 0:
+            break
+    return result
+
+def do_command(command):
+    """Send one command, and return the response."""
+    send_command(command)
+    response = get_response()
+    print("Rcvd: <<< \n" + response)
+    return response
+
+def databend_img():
+    # Select track in project.
+    do_command('SelectAll:')
+
+    # Start moving the playhead past the beginning of the track (the image file header).
+    do_command('PlayStopSelect:')
+
+    # Let audio play to avoid manipulating header.
+    time.sleep(1)
+    do_command('PlayStopSelect:')
+
+    # Do the databend.
+    # TODO: parameterize the following command so that we can pass a preset
+    # into this function to allow the script to do different databends.
+    
+    # The following are exmples of commands that can be used to manipulate the audio.
+    # do_command('Amplify:Ratio=0.7')
+    # do_command('BassAndTreble:Bass=0.5 Treble=-0.5')
+    do_command('ChangeTempo: Percentage=3')
+    # TODO: When audacity supports direct exporting to raw audio files,
+    # we can export the raw audio file here.
+    # do_command('Export2:Filename="./testModRaw.raw"')
+
+databend_img()
